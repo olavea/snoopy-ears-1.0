957f7d3092ffe3183503183ca97fe270cb253d8c
Ola Vea <ola@olavea.com>
2021-01-25 18:21:09 +0100

    siteUrl: `https://snoopyears10master.gtsb.io/`,  #6

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ require("dotenv").config({
 module.exports = {
   siteMetadata: {
     title: "Snoopy Ears 1.0",
-    siteUrl: `https://snoopyears.gtsb.io/`,
+    siteUrl: `https://snoopyears10master.gtsb.io/`,
   },
   plugins: [
     {
