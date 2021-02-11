d9b7d003ad2e5d70de98fc078e1e8c0dea0fd9c3
Ola <ola@olavea.com>
2021-02-11 13:34:20 +0100

    <MrPragma /> in index #14

---
+++ src/components/MrPragma.js
@@ -0,0 +1,6 @@
+// import React from 'react'
+// import theme from '../gatsby-plugin-theme-ui'
+// console.log(theme)
+// export const MrPragma = () => {
+//   return <div>MrPragma</div>
+// }
--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,6 +1,9 @@
 export default {
     colors: {
-      text: "#FFFFFF",
-      background: "#131127",
+      text: "#ff1616",
+      background: "#ffde59",
     },
+    space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
   }
+
+  // background: "#fffaf0",
