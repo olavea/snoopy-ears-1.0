281f0d9fac3503da1f15a4cbc3a1c003bdf9f0f4
Ola <ola@olavea.com>
2021-02-15 10:03:28 +0100

    contact-us form with Formium works #15

---
+++ src/pages/contact.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import { FormiumForm } from '@formium/react';
+import { formium } from '../lib/formium';
+export default function FeedbackForm({ data }) {
+  const [success, setSuccess] = React.useState(false);
+  if (success) {
+    return <div>Thank you! Your response has been recorded.</div>;
+  }
+  return (
+    <FormiumForm
+      data={data.formiumForm}
+      onSubmit={async (values) => {
+        // Send form values to Formium
+        await formium.submitForm('contact-us', values);
+        setSuccess(true);
+      }}
+    />
+  );
+}
+export const query = graphql`
+  {
+    formiumForm(slug: { eq: "contact-us" }) {
+      id
+      createAt
+      name
+      projectId
+      schema
+      slug
+      updateAt
+      version
+    }
+  }
+`;
