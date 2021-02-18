5e7fc292a2bfe7a4a6167e1b25bf637839977c11
Ola <ola@olavea.com>
2021-02-18 08:57:33 +0100

    siteUrl #13

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
     title: "Snoopy Ears 1.0",
     titleTemplate: "%s · The Real Hero",
     description: "Prototype",
+    siteUrl: `https://snoopyears.gtsb.io`,
     url: `https://snoopyears.gtsb.io`,
     image: "/images/icon.png", // Path to your image you placed in the 'static' folder
     twitterUsername: "@olaholstvea",
