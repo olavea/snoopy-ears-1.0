43b5877030e294514b3f655fbf8271f61d850098
Ola <ola@olavea.com>
2021-03-08 10:55:09 +0100

    <link to="/">No? Go home to Slash 🎩</link>

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,53 +1,22 @@
 import * as React from "react"
 import { Link } from "gatsby"
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+    <>
+    <div>
+     <h1>Welcome to Valhalla!
+     </h1>
+     <p>Well you need to cross the bridge. But first two quick questions.</p>
+     <ol>
+     <li>Did you die honorably in coding combat?</li>
+     <li>Did you die still holding your pasSword in your hand?</li>
+     <li></li>
+     </ol>
+     <link to="/">No? Go home to Slash 🎩</link>
+    </div>
+
+    </>
   )
 }
 
