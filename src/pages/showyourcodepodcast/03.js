e4df347b3c690aa420ebe1611c1f48b8001be2a8
Ola <ola@olavea.com>
2021-03-31 16:48:43 +0200

    use-sound and audacity #46

---
+++ src/pages/showyourcodepodcast/03.js
@@ -0,0 +1,49 @@
+import React from "react"
+import { Button, Flex, Box } from "theme-ui"
+
+
+import useSound from "use-sound"
+import laugh1 from "../../assets/podcasts/3-show-your-code-episode-3_recording-1_2021-03-31-Podcast-ready-olavea.mp3"
+// import laugh2 from "../assets/laughs/Laugh2LillianBenedicte2020.mp3"
+// import laugh3 from "../assets/laughs/Laugh3LillianJUNE_30_2016_Trampolinelykke_paa_landet_hos_babywildelvs.mp3"
+//Users/olavea/Desktop/Develop/100days/snoopy-ears-1.0/snoopy-ears-1.0/src/assets/podcasts/3-show-your-code-episode-3_recording-1_2021-03-31-Podcast-ready-olavea.mp3
+//snoopy-ears-1.0/src/assets/podcasts/3-show-your-code-episode-3_recording-1_2021-03-31-Podcast-ready-olavea.mp3
+
+export default function Home() {
+    const [play, {stop}] = useSound(laugh1);
+    return (
+        <>
+      <Flex
+        bg=""
+        sx={{
+          height: "100vh",
+          width: "100vw",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Box>
+          <Button
+            onClick={play}
+            // What should the user do to stop laughter?
+            // Frown? Say "STOP laughing!"
+            sx={{ height: "80vh", width: "clamp(30px, 30vw, 600px)" }}
+          >
+            <h1>Show Your Code </h1>
+            <h2>Episode 3 </h2>
+            <h2>Tom Erik Støwer and</h2>
+            <h2>Ola Vea</h2>
+
+          </Button>
+        </Box>
+      </Flex>
+        <a href="https://gist.github.com/testower/190e8738ea5913ccce503bcc7774b60c">
+            Tom Erik's gist
+        </a>
+        <a href="https://tomerikstower.com/">
+            Tom Erik's blog
+        </a>
+
+      </>
+    )
+  }
