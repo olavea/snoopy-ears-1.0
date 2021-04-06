cc1609229006cc7f2ebd4c9e792e1f9a8807e3d6
Ola <ola@olavea.com>
2021-04-06 10:30:10 +0200

    Proc. & render images #56

--- src/pages/{Mdx.slug}.js
+++ src/pages/{Mdx.slug}.js
@@ -2,14 +2,16 @@ import {graphql} from "gatsby"
 import React from "react"
 import Layout from "../components/Layout"
 import {MDXRenderer} from "gatsby-plugin-mdx"
-
+import {GatsbyImage, getImage} from "gatsby-plugin-image"
 
 const showTale = ({data}) => {
   const treasure = data.mdx
+  const image = getImage(treasure.frontmatter.image)
   return (
     <>
       <Layout>
         <article>
+          <GatsbyImage image={image} alt={treasure.frontmatter.imageAlt} />
 
       <h1>{treasure.frontmatter.title}</h1>
       <MDXRenderer>
@@ -28,6 +30,12 @@ export const query =graphql`
         mdx(id: {eq: $id}){
             frontmatter {
                 title
+                image {
+                  childImageSharp {
+                    gatsbyImageData(layout: CONSTRAINED)
+                  }
+                }
+                imageAlt
             }
             body
         }
