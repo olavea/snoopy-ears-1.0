a1bc41c574d1aa9a459ff2bb26fb62e28dc60086
Ola <ola@olavea.com>
2021-04-06 13:46:06 +0200

    <SEO title={`🎩  is Home, but you're NOT.`} /> #60

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
+import SEO from "../components/seo"
 
 const NotFoundPage = () => {
   return (
