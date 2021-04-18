1eb3e8be9625f3c78383cad73fe7d44872a5667c
Ola <ola@olavea.com>
2021-04-18 08:35:57 +0200

    adde 🏴‍☠️ #78

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,5 +2,5 @@ import React from 'react'
 // import Layout from './src/components/Layout'
 
 export function wrapPageElement({ element, props }) {
-     return <div {...props }>{element}<span role="img">💩</span></div>;
+     return <div {...props }>{element}<span role="img" alt="vulcan salute emoji, smiling kitty emoji and pirate flag emoji">🖖😺🏴‍☠️</span></div>;
 }
