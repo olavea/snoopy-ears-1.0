623d30120e94962caaf5ffacd553d574f068ff1d
Ola <ola@olavea.com>
2021-10-09 13:00:37 +0200

    Fix: Typo Layout not layout in gatsby-browser+ssr

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,5 @@
 const React = require("react");
-const { default: Layout } = require("./src/components/layout");
+const { default: Layout } = require("./src/components/Layout");
 
 
 exports.wrapPageElement = ({ element, props }) => {
