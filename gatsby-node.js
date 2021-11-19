b16c68b3851d0bdb0f96bef3c81d53f11e2938b3
Ola <ola@olavea.com>
2021-11-19 10:49:20 +0100

    fix: dsg an email

---
+++ gatsby-node.js
@@ -0,0 +1,25 @@
+// const path = require("path")
+
+
+// async function turnPizzasIntoPages({ graphql, actions }) {
+//     // 1.
+//     //const pizzaTemplate = path.resolve("component-path")
+//     // 2.
+//     // 3. Loop over each pizza and create a page for that pizza
+//     const { createPage } = actions
+//     // createPage({
+//     //     path: "page-path",
+//     //     component: "component-path",
+//     //     context: {},
+//     //    defer: true,
+//     // })
+// }
+
+
+// export function createPages(params)  {
+//   //  await turnPizzasIntoPages(params);
+// }
+
+// exports.createPages = async (params) => {
+//     await turnPizzasIntoPages(params);
+// }
