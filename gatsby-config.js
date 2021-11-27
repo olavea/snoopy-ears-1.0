31d6bcceda289f4e0b4ed0382c85df384efbf683
Ola <ola@olavea.com>
2021-11-27 08:54:31 +0100

    fix: local images are working

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,25 @@ module.exports = {
     siteUrl: "https://demo.raae.codes",
     title: "@raae demo starter",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud", "@raae/gatsby-plugin-new-css"],
+  plugins: [
+    "gatsby-plugin-gatsby-cloud",
+    "@raae/gatsby-plugin-new-css",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/content`,
+      },
+    },
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `images`,
+    //     path: path.join(__dirname, `src`, `images`),
+    //   },
+    // },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-image`
+  ],
 };
